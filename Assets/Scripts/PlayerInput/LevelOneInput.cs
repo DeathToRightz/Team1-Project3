449539065c47539e75a90ed4c7d466776b3e1e80
BasedPlant <146005173+BasedPlant@users.noreply.github.com>
2024-11-06 09:26:28 -0500

    LevelOneMovement made

---
+++ Assets/Scripts/PlayerInput/LevelOneInput.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelOneInput : MonoBehaviour
+{
+    [SerializeField] private Transform[] movePoints; //Array of points to move between
+    [SerializeField] private float moveSpeed = 5f;
+    private int currentPointIndex = 0; //Current target point
+
+    [SerializeField] private BoxCollider movementArea; //Define a box collider as the allowed movement area
+    [SerializeField] private bool shouldMoveToPoint = false; // Controls when MoveToPoint() should be called
+
+    [SerializeField] private Animator animator;
+    [SerializeField] private string animationName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        PlayAndDisableAnimator();
+    }
+
+    public void PlayAndDisableAnimator()
+    {
+        animator.Play(animationName); // Play the animation
+        StartCoroutine(DisableAnimatorAfterAnimation());
+    }
+
+    private IEnumerator DisableAnimatorAfterAnimation()
+    {
+        // Wait until the animation has finished playing
+        yield return new WaitForSeconds(2.2f);
+        animator.enabled = false; // Disable the Animator component
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Calls MoveToPoint() if shouldMoveToPoint is true
+        if (IsPlayerInsideMovementArea())
+        {
+            MoveToPoint();
+            //shouldMoveToPoint = false;
+        }
+    }
+
+    // Move the player towards the target point
+    private void MoveToPoint()
+    {
+        if (movePoints.Length == 0) return;
+
+        Transform targetPoint = movePoints[currentPointIndex];
+        transform.position = Vector3.MoveTowards(transform.position, targetPoint.position, moveSpeed * Time.deltaTime);
+    }
+
+    public void MoveLeft()
+    {
+        // Only move left if we are not at the first point (index 0)
+        if (currentPointIndex > 0)
+        {
+            currentPointIndex--;
+            shouldMoveToPoint = true;
+        }
+    }
+
+    public void MoveRight()
+    {
+        // Only move right if we are not at the last point (index movePoints.Length - 1)
+        if (currentPointIndex < movePoints.Length - 1)
+        {
+            currentPointIndex++;
+            shouldMoveToPoint = true;
+        }
+    }
+
+    private bool IsPlayerInsideMovementArea()
+    {
+        return movementArea.bounds.Contains(transform.position);
+    }
+}
---
+++ Assets/Scripts/PlayerInput/LevelOneInputManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class LevelOneInputManager : MonoBehaviour
+{
+    LevelOneInput levelOneInputScript;
+    PlayerInput playerInput;
+    public PlayerInput.LevelOneActions levelOneAction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        playerInput = new PlayerInput();
+        levelOneAction = playerInput.LevelOne;
+        levelOneInputScript = gameObject.GetComponent<LevelOneInput>();
+
+        levelOneAction.MoveLeft.performed += ctx => levelOneInputScript.MoveLeft();
+        levelOneAction.MoveRight.performed += ctx => levelOneInputScript.MoveRight();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        levelOneAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        levelOneAction.Disable();
+    }
+}
