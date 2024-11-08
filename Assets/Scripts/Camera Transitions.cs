93a1d91f0167c991bc176ba4e2a1c377664c5764
DeathToRightz <142912611+DeathToRightz@users.noreply.github.com>
2024-11-08 17:14:59 -0500

    Made Camera Transitions for Kay

--- Assets/Scripts/Camera Transitions.cs
+++ Assets/Scripts/Camera Transitions.cs
@@ -2,8 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-
+using Cinemachine;
 public class CameraTransitions : MonoBehaviour
 {
- 
+    [SerializeField]GameObject[] cameras;
+
+
+    private void Start()
+    {
+       
+        StartCoroutine(ChangeCameraView(cameras));
+    }
+
+
+    IEnumerator ChangeCameraView(GameObject[] incomingArray)
+    {
+       int i = 0;
+            while (i <= incomingArray.Length-1 )
+            {
+                yield return new WaitForSeconds(4);
+                Debug.Log("Switch" + i);
+                incomingArray[i].SetActive(false);
+            i++;
+            }
+            
+        
+        Debug.Log("Finish");
+        incomingArray[0].SetActive(true);
+    }
 }
