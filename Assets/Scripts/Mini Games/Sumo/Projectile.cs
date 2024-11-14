8b7853cd1bb5cebe11bd88caeb63343ae83e6b2c
DeathToRightz <142912611+DeathToRightz@users.noreply.github.com>
2024-11-14 18:20:16 -0500

    Fixed a bug with cannon

--- Assets/Scripts/Mini Games/Sumo/Projectile.cs
+++ Assets/Scripts/Mini Games/Sumo/Projectile.cs
@@ -4,23 +4,11 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    private Rigidbody _rb;
-
-   public float shotPower = 500f;
-
-    private void Awake()
-    {
-        _rb = GetComponent<Rigidbody>();
-    }
     void Start()
     {
-        _rb.AddForce(Vector3.right * shotPower);
+
         Destroy(gameObject, 4f);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
--- Assets/Scripts/Mini Games/Sumo/canon.cs
+++ Assets/Scripts/Mini Games/Sumo/canon.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class canon : MonoBehaviour
@@ -7,6 +8,8 @@ public class canon : MonoBehaviour
     [SerializeField] GameObject[] _projectiles;
 
     [SerializeField] Transform _shootPos;
+
+    [SerializeField] float _shootPower;
 
     // Update is called once per frame
     void Update()
@@ -22,27 +25,40 @@ public class canon : MonoBehaviour
     private void Shoot(int incomingRandomNumber)
     {
         GameObject chosenFruit = null;
-        switch (Random.Range(0, incomingRandomNumber))
-        {           
+        
+        Rigidbody _rb = null;
+        
+        switch (Random.Range(0, _projectiles.Length))
+        {
             case 0:
-            Debug.Log("Apple");
-            chosenFruit = Instantiate(_projectiles[0], _shootPos.position, Quaternion.identity);
-            break;
-        case 1:
-            Debug.Log("Orange");
-            chosenFruit = Instantiate(_projectiles[1], _shootPos.position, Quaternion.identity);
-            break;
-        case 2:
-            Debug.Log("Blueberry");
-            chosenFruit = Instantiate(_projectiles[2], _shootPos.position, Quaternion.identity);
-            break;
-        case 3:
-            Debug.Log("Watermelon");
-            chosenFruit = Instantiate(_projectiles[3], _shootPos.position, Quaternion.identity);
-            break;
-        default:
-            Debug.Log("Person!");
-            break;
+                Debug.Log("Apple");
+                chosenFruit = Instantiate(_projectiles[0], _shootPos.position,Quaternion.identity);
+                _rb = chosenFruit.GetComponent<Rigidbody>();
+                _rb.AddForce(_shootPos.forward * _shootPower);
+               
+                
+                break;
+            case 1:
+                Debug.Log("Orange");
+                chosenFruit = Instantiate(_projectiles[1], _shootPos.position, Quaternion.identity);
+                _rb = chosenFruit.GetComponent<Rigidbody>();
+                _rb.AddForce(_shootPos.forward * _shootPower);
+                break;
+            case 2:
+                Debug.Log("Blueberry");
+                chosenFruit = Instantiate(_projectiles[2], _shootPos.position, Quaternion.identity);
+                _rb = chosenFruit.GetComponent<Rigidbody>();
+                _rb.AddForce(_shootPos.forward * _shootPower);
+                break;
+            case 3:
+                Debug.Log("Watermelon");
+                chosenFruit = Instantiate(_projectiles[3], _shootPos.position, Quaternion.identity);
+                _rb = chosenFruit.GetComponent<Rigidbody>();
+                _rb.AddForce(_shootPos.forward * _shootPower);
+                break;
+            default:
+                Debug.Log("Person!");
+                break;
         }
     }
 }
