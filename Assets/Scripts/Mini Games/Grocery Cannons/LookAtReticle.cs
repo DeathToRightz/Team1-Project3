fd93d77527206419e955d345a0b08df8441d7e39
DeathToRightz <142912611+DeathToRightz@users.noreply.github.com>
2024-11-23 20:36:54 -0500

    Cannon Shoots

--- Assets/Scripts/Mini Games/Grocery Cannons/LookAtReticle.cs
+++ Assets/Scripts/Mini Games/Grocery Cannons/LookAtReticle.cs
@@ -4,12 +4,31 @@ using UnityEngine;
 
 public class LookAtReticle : MonoBehaviour
 {
+    public float powa = 100f;
     public Transform target;
-
-    // Update is called once per frame
+    public ParticleSystem _smokeParticle;
+    public GameObject _projectile;
+    private Vector3 _direction;
+    public Transform _shootPoint;
     void Update()
     {
+        _direction = target.position - transform.position;
         //transform.TransformDirection(Vector3.forward);
         transform.LookAt(target);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            _smokeParticle.Play();
+            Shoot(_projectile);
+        }
+    }
+
+    private void Shoot(GameObject incomingGameObject)
+    {
+        GameObject projectile = null;
+        projectile = Instantiate(incomingGameObject,_shootPoint);
+        Rigidbody rb = projectile.GetComponent<Rigidbody>();
+        rb.AddForce(_direction * powa);
+
     }
 }
